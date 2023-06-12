96e2ca20f333696807ac156b6d859257ba6a044b
Erick El nino <erick.tshimbombo@yahoo.com>
2023-06-12 03:53:46 +0200

    update

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-console.log('testting');
-console.log("Hello world");
+const {format} = require('date-fns');
+const { v4: uuid } = require('uuid');
+
+console.log(format(new Date(), 'yyyy-MM-dd'));
+
+console.log(uuid());
