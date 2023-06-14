3c13ce545e29a6760be205c77e8b4f19fdc4f1aa
Erick El nino <erick.tshimbombo@yahoo.com>
2023-06-15 01:13:08 +0200

    update

--- server.js
+++ server.js
@@ -6,18 +6,21 @@ class MyEvent extends Event { };
 
 const myEvent = new MyEvent();
 
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const { da } = require('date-fns/locale');
+const fspromise = require('fs').promises;
 
 
 //  listen to the event
-myEvent.on('log',(mess) =>{
+// myEvent.on('log',(mess) =>{
 
-    return myLogEvents(mess);
-});
-
-
-
-setTimeout(() =>{
-   myEvent.emit('log','i was gonna tell your mom');
-},2000);
+//     return myLogEvents(mess);
+// });
 
 
+
+// setTimeout(() =>{
+//    myEvent.emit('log','i was gonna tell your mom');
+// },2000);
